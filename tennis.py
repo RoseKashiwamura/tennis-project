57ad6ec4cd36be595c55da8f9c7f43bee9c4b0f5
SayaNagae <sayanagae@gmail.com>
2024-12-01 20:20:06 -0800

    added background color

--- tennis.py
+++ tennis.py
@@ -2,10 +2,12 @@ import pygame, sys
 pygame.init() 
 clock = pygame.time.Clock()
 
+green = (97, 190, 22)
 screen_width = 3840
 screen_height = 2160
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('ap comp sci tennis')
+screen.fill(green)
 
 while True:
     for event in pygame.event.get():
