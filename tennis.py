7d15f9e5fa928d84777bc2cf98874371201d027b
RoseKashiwamura <Osadakashi@gmail.com>
2024-12-08 21:26:45 -0800

    1. Rectangle objects determining court boundaries 2. Tennis racket and tennis ball image

--- tennis.py
+++ tennis.py
@@ -2,17 +2,30 @@ import pygame, sys
 pygame.init() 
 clock = pygame.time.Clock()
 
-green = (97, 190, 22)
+#colors
+bgcolor = (232, 0, 255)
+playersidecolor = (97, 190, 22)
+cpusidecolor = (50, 171, 199)
+oobcolor = (173, 40, 40)
+
+#bg screen
 screen_width = pygame.display.Info().current_w
 screen_height = pygame.display.Info().current_h
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)   
 pygame.display.set_caption('ap comp sci tennis')
-screen.fill(green)
+screen.fill(bgcolor)
+
+#objects
 
 court = pygame.image.load("Images/tennis-court.png").convert_alpha()
-court = pygame.transform.smoothscale(court,(screen_width,1.5*screen_height))
-
-screen.blit(court, (0, -screen_height/4))
+court = pygame.transform.smoothscale(court,(0.9*screen_width,1.5*screen_height))
+playerside = pygame.draw.rect(screen, playersidecolor, pygame.Rect(0, 0, screen_width/2, screen_height))
+cpuside = pygame.draw.rect(screen, cpusidecolor, pygame.Rect(screen_width/2, 0, screen_width/2, screen_height))
+oobleft = pygame.draw.rect(screen, oobcolor, pygame.Rect(0, 0, screen_width/20, screen_height))
+oobright = pygame.draw.rect(screen, oobcolor, pygame.Rect(0.95*screen_width, 0, screen_width/20, screen_height))
+oobtop = pygame.draw.rect(screen, oobcolor, pygame.Rect(0, 0, screen_width, screen_height/7.2))
+oobbottom = pygame.draw.rect(screen, oobcolor, pygame.Rect(0, 0.85*screen_height, screen_width, screen_height/7.2))
+screen.blit(court, (screen_width/20, -screen_height/4))
 
 while True:
     for event in pygame.event.get():
